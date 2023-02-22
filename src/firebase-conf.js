819a3a913478ef72997c72c2395c26a3c1045f3f
Debajyoti Halder <debajyotih@iitbhilai.ac.in>
2023-02-22 16:08:36 -0500

    changed to sign in with popup

--- src/firebase-conf.js
+++ src/firebase-conf.js
@@ -23,32 +23,8 @@ const provider = new GoogleAuthProvider();
 
 // sign in with redirect
 
-export const googleSignIn = () => {
-    signInWithRedirect(auth, provider)
-        .then((result) => {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            // The signed-in user info.
-            const user = result.user;
-            window.user = user;
-
-            window.location.href = '/home';
-            // ...
-        }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
-        });
-}
-
 // export const googleSignIn = () => {
-//     signInWithPopup(auth, provider)
+//     signInWithRedirect(auth, provider)
 //         .then((result) => {
 //             // This gives you a Google Access Token. You can use it to access the Google API.
 //             const credential = GoogleAuthProvider.credentialFromResult(result);
@@ -70,6 +46,30 @@ export const googleSignIn = () => {
 //             // ...
 //         });
 // }
+
+export const googleSignIn = () => {
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            window.user = user;
+
+            window.location.href = '/home';
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        });
+}
 
 onAuthStateChanged(auth, async(user) => {
     if (user) {
